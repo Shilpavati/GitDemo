27c01e25197980f382e2d5401f785c39b57a0675
Shilpavati <shilpavati@gmail.com>
2021-03-23 17:24:14 +0530

    first commit

---
+++ src/test/java/QAClickAcademy/MavenJava/RestAPITest.java
@@ -0,0 +1,16 @@
+package QAClickAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void postJIRA() {
+		System.out.println("postJIRA");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
