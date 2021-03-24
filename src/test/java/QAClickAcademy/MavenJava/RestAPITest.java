e2d0173c267d5b28a36a377714a0d2413998626e
Shilpavati <shilpavati@gmail.com>
2021-03-24 15:54:39 +0530

    American guy commit

--- src/test/java/QAClickAcademy/MavenJava/RestAPITest.java
+++ src/test/java/QAClickAcademy/MavenJava/RestAPITest.java
@@ -10,6 +10,11 @@ public class RestAPITest {
 		System.out.println("postJIRA1");
 		System.out.println("postJIRA2");
 		System.out.println("postJIRA3");
+		
+		System.out.println("postJIRA4");
+		System.out.println("postJIRA5");
+		System.out.println("postJIRA6");
+		System.out.println("postJIRA7");
 	}
 
 	@Test
